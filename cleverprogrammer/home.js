965b9fff7c09482aef8e2f0022078f1d400a16c7
lishu gupta <lishugupta652@gmail.com>
2020-08-19 15:44:54 +0530

    function

--- cleverprogrammer/home.js
+++ cleverprogrammer/home.js
@@ -3,3 +3,34 @@
 
 // var age = prompt("What is your age");
 // document.getElementById("someText").innerText = age;
+
+/* Function 
+1. Create a function 
+2. Call a function
+*/
+// function fun() {
+//   alert("This is the function ");
+// }
+
+// fun();
+
+/* Function: Let's create a function which takes your name and return Hello followed by your name
+
+for example
+
+Name : "Lishu"
+Return : "Hello Lishu"
+*/
+// function greeting() {
+//   var name = prompt("What is your name ");
+//   var result = "Hello " + name;
+//   alert(result);
+// }
+// greeting();
+
+// Funciton with arguments
+// function add(num1, num2) {
+//   return num1 + num2;
+// }
+// var res = add(20, 20);
+// console.log(res);
