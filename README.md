3c3a5d4dbc9450185087a8d154217e28260c10df
lishu gupta <lishugupta652@gmail.com>
2020-08-19 21:21:06 +0530

    color app

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # pe-lt-javascript
 
-## Learning Projects javascript 
+## Learning Projects javascript
 
-## Branch 1 
-- 
+## Codicts
---
+++ color/javascript/main.js
@@ -0,0 +1,16 @@
+const changeBackground = () => {
+  colors = ["red", "blue", "green", "rgba(100, 100, 100, 0.9)"];
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+  console.log(randomColor);
+  document.body.style.backgroundColor = randomColor;
+};
+
+const changeHex = () => {
+  colors = [1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
+  var hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += colors[Math.floor(Math.random() * colors.length)];
+  }
+  console.log(hexColor);
+  document.body.style.backgroundColor = hexColor;
+};
